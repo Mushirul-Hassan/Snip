b99c5ecfd26b23aac92d502582b456e36726cc8b
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-04 21:37:27 +0530

    added nano id for url generation

--- backend/models/urlModel.js
+++ backend/models/urlModel.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
 const projectSchema = new mongoose.Schema({
-  user: {
-    type: String,
-    required: true,
-  },
+  // user: {
+  //   type: String,
+  //   required: true,
+  // },
 
   url: {
     type: String,
     required: true,
   },
 
-  shortUrl: { type: String },
+  nano_id: { type: String, default: nano.id },
 });
 
 export const Url = mongoose.model("url", projectSchema);
