3eeeb970090c91f8c6683ab3492e8f65d6c7f443
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-04 21:38:05 +0530

    removed shorturl calling again since it was basically calling of thw orifginal url

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-router.post("/user");
+// router.post("/user");
+// router.get("/user/:id");
 router.post("/url");
 router.get("/url/:id");
-router.get("/user/:id");
-router.get("/shortUrl");
+// router.get("/shortUrl");
