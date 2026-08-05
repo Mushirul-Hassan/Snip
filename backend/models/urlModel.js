ed37805aa4cdee3c51b21b1d29d7a01c59ac8a2a
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-05 20:39:40 +0530

    added few express in server for parsing of raw request data

--- backend/models/urlModel.js
+++ backend/models/urlModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { nanoid } from 'nanoid'
 
 const projectSchema = new mongoose.Schema({
   // user: {
---
+++ backend/server.js
@@ -0,0 +1,5 @@
+import express from 'express';
+// import urlContr
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
