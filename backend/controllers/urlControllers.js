d43c4a0437ece72bc6e056ec7f15f477789b528d
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-05 20:40:01 +0530

    destructured the url json

--- backend/controllers/urlControllers.js
+++ backend/controllers/urlControllers.js
@@ -2,11 +2,11 @@ import express from "express";
 import { Url } from "../models/urlModel";
 
 export const createUrl = (req, res) => {
+  const { url } = req.body;
 
-    
+  res.send("here i will create a shoert url");
 };
 
 export const getUrl = (req, res) => {
-
-
+  res.send("here i will get the  url");
 };
