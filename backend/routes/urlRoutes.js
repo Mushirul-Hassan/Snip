5e9b027b76711b4ef6a61bbc19fd040cf3cbb3bf
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-07 21:45:39 +0530

    added createurl in route

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import { createUrl } from '../controllers/urlControllers';
 const router = express.Router();
 
 // router.post("/user");
 // router.get("/user/:id");
-router.post("/url");
+router.post("/url", createUrl);
 router.get("/url/:id");
 // router.get("/shortUrl");
