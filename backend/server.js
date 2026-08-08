db63b37372ed41f77104595d55c9566431ee02a7
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-08 18:34:38 +0530

    initialized express fully

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 import express from 'express';
 // import urlContr
 
+const app = express();
+const PORT = 3000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+ 
+app.get("/", (req, res) => {
+  response.send({ message: "Hello from an Express API!" });
+});
+ 
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
