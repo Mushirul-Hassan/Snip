dd2138e6e262c4e5c530219758f95f70c1f6f199
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-09 20:11:30 +0530

    fixed the file not found error

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUrl, getUrl } from '../controllers/urlControllers';
+import { createUrl, getUrl } from '../controllers/urlControllers.js';
 const router = express.Router();
 
 // router.post("/user");
