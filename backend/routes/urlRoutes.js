49b78340a08c0e8331182ac0c9a5e4e6162792fa
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-09 19:52:40 +0530

    connected routes and server file through userRouterand changed the api nomenclature

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 // router.post("/user");
 // router.get("/user/:id");
-router.post("/url", createUrl);
-router.get("/url/:id", getUrl);
+router.post("/create", createUrl);
+router.get("/:id", getUrl);
 // router.get("/shortUrl");
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import  dotenv  from "dotenv";
-// import urlContr
+import urlRouter from "./routes/urlRoutes.js";
 
 dotenv.config();
 connectDB();
+
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/url", urlRouter);
 
 app.get("/", (req, res) => {
   res.send({ message: "Hello from an Express API!" });
