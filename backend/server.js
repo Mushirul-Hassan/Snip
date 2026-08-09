a7dfac82582caad5b6280946d4357360c4f5d27b
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-09 19:07:14 +0530

    added dotenv and mongo config in server

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-import express from 'express';
+import express from "express";
+import { connectDB } from "./config/db";
+import { dotenv } from "dotenv";
 // import urlContr
 
+dotenv.config();
+connectDB();
 const app = express();
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
- 
 app.get("/", (req, res) => {
   res.send({ message: "Hello from an Express API!" });
 });
- 
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
