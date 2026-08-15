5ed18aae03608be82fa136b976f47a4469279fb7
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-15 15:14:49 +0530

    added update url route for working upon the update feature

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createUrl, getUrl } from '../controllers/urlControllers.js';
+import { createUrl, getUrl, updateUrl } from '../controllers/urlControllers.js';
 const router = express.Router();
 
 // router.post("/user");
 // router.get("/user/:id");
 router.post("/create", createUrl);
 router.get("/:nano_id", getUrl);
+router.put("/:nano_id", updateUrl);
 // router.get("/shortUrl");
 export default router;
