2cbd17d515124eb16b24156cc74e6912d8012965
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-16 02:28:26 +0530

    using nano_id for updation

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 // router.get("/user/:id");
 router.post("/create", createUrl);
 router.get("/:nano_id", getUrl);
-router.patch("/:id", updateUrl);
+router.patch("/:nano_id", updateUrl);
 // router.get("/shortUrl");
 export default router;
