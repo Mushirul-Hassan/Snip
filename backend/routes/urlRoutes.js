2559dcb7d1d61a7f3535942b11a1b2ed03016327
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-18 00:21:17 +0530

    making ways for deletion of url

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUrl, getUrl, updateUrl } from '../controllers/urlControllers.js';
+import { createUrl, getUrl, updateUrl, deleteUrl } from '../controllers/urlControllers.js';
 const router = express.Router();
 
 // router.post("/user");
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/create", createUrl);
 router.get("/:nano_id", getUrl);
 router.patch("/:nano_id", updateUrl);
+router.delete(":nano_id", deleteUrl);
 // router.get("/shortUrl");
 export default router;
