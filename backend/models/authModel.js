fe91015eb1c340d273bba9e194874ad194162d10
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-09-03 19:01:52 +0530

    made email and password unique

--- backend/models/authModel.js
+++ backend/models/authModel.js
@@ -9,10 +9,12 @@ const authSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
       required: true,
+      unique: true,
     },
   },
   { timestamps: true },
